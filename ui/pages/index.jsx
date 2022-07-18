6860bcaadd3e93136d5f57f91bce26ebffbca849
Elisha Bere <elishabere4@gmail.com>
2022-07-18 09:00:35 +0200

    update txt

--- ui/pages/index.jsx
+++ ui/pages/index.jsx
@@ -204,7 +204,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <h1 className={styles.title}>Welcome to Crypto Bros!</h1>
           <div className={styles.description}>
             Its an NFT collection for developers in Crypto.
           </div>
